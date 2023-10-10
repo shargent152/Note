216b9bd2458b7f73a4961ac1da85aadd42e2a6ab
Trevor Peevers <shargent152@gmail.com>
2023-10-09 23:14:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*val dataset = arrayOf("test", "test2", "pleasefortheloveofgodwork")
+        val customAdapter = CustomAdapter(dataset)
+        val recyclerView : RecyclerView = findViewById(R.id.recycle)
+        recyclerView.adapter = customAdapter*/
+
+    }
+
+    fun Addnote(view: View) {
+
+    }
+}
